8a88c314633fa4565d0fa50477a0cc355ce550d0
Willian T <willian0404@hotmail.co.jp>
2023-06-22 22:00:39 -0300

    Aula completa

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { FormsModule } from '@angular/forms';
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
+import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarPensamentoComponent,
+    ListarPensamentosComponent,
+    PensamentoComponent,
+    ExcluirPensamentoComponent,
+    EditarPensamentoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,45 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { PensamentoService } from '../pensamento.service';
+import { Pensamento } from './../pensamento';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrls: ['./excluir-pensamento.component.css']
+})
+export class ExcluirPensamentoComponent implements OnInit {
+
+  Pensamento :Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento)=>{
+      this.Pensamento = pensamento
+    })
+  }
+
+  excluirPensamento(){
+    if(this.Pensamento.id){
+      this.service.excluir(this.Pensamento.id!).subscribe(()=>{
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
+  }
+
+  cancelar(){
+    this.router.navigate(['/listarPensamento'])
+  }
+
+
+}
